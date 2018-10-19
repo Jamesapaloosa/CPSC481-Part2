06629db999bd4366d6d22f59cc55652d2ec36c04
VIKI\James <jamesapaloosa1@gmail.com>
2018-10-19 12:29:33 -0600

    updated order option

--- CPSC 481/Menu.xaml.cs
+++ CPSC 481/Menu.xaml.cs
@@ -23,9 +23,19 @@ namespace CPSC_481
         {
             InitializeComponent();
             this.listBox.Items.Add("Burger");
+            this.listBox.Items.Add("Chicken Soup");
+            this.listBox.Items.Add("Boston Beef Cake");
+            this.listBox.Items.Add("Rib eye Steak");
+            this.listBox1.Items.Add("Whisky 12X - $126.00");
+            this.TotalPriceLabel.Content = "$126.00";
         }
 
         private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }

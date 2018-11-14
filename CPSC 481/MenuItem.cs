6e9985bdccd4deb780db6dd22fa02dd4ee12c431
ttdjohns <32712899+ttdjohns@users.noreply.github.com>
2018-11-14 10:13:59 -0700

    Add files via upload

--- CPSC 481/MenuItem.cs
+++ CPSC 481/MenuItem.cs
@@ -12,14 +12,34 @@ namespace CPSC_481
         public decimal price { get; set; }
         public MenuItem.Type type { get; set; }
         public String imageName { get; set; }
+        public string[,,] options = new string[5, 6, 2];
 
-        public MenuItem(String name, String description, decimal price, MenuItem.Type type, String imageName)
+
+        public MenuItem(String name, String description, decimal price, MenuItem.Type type, String imageName, string[,,] itemOptions)
         {
             this.name = name;
             this.description = description;
             this.price = price;
             this.type = type;
             this.imageName = imageName;
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (!(String.IsNullOrEmpty(itemOptions[i, j, 0])) && !(String.IsNullOrEmpty(itemOptions[i, j, 0])))
+                        {
+                            //System.Diagnostics.Debug.WriteLine("name = " + itemOptions[i, j, 0] + " price = " + itemOptions[i, j, 1]);
+                            options[i, j, 0] = itemOptions[i, j, 0];
+                            options[i, j, 1] = itemOptions[i, j, 1];
+                        }
+                    }
+                }
+            } catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
         }
     }
 }
